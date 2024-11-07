e07de0d901d9344d51db8107df05df85c996f755
aziz0123 <aziz.belhadjkhaled@esprit.tn>
2024-11-07 03:17:27 +0100

    adding tests

--- src/test/java/tn/esprit/tpfoyer/FoyerServiceImplTest.java
+++ src/test/java/tn/esprit/tpfoyer/FoyerServiceImplTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import tn.esprit.tpfoyer.entity.Etudiant;
@@ -24,7 +25,7 @@ import java.time.LocalDate;
 import static org.junit.jupiter.api.Assertions.*;
 @ExtendWith(SpringExtension.class)
 @TestMethodOrder(MethodOrderer. OrderAnnotation.class)
-@ActiveProfiles("test")
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 @SpringBootTest
 public class FoyerServiceImplTest {
 
